4cd5185b9f4ed1f8b08bddfb1c9558fd362f726c
Ghislain Leblanc <ghisleb@me.com>
2026-07-19 20:59:24 -0400

    feat: Add NewTopUI app project

---
+++ NewTopUI/NewTopUI/NewTopUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTopUIApp.swift
+//  NewTopUI
+//
+//  Created by Ghislain Leblanc on 2026-07-19.
+//
+
+import SwiftUI
+
+@main
+struct NewTopUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
